eb391acefd655ea677afcf07fc5a5e9a47615834
JurWijn <jurwijn96@gmail.com>
2025-04-10 11:44:06 +0200

    up to CH3 L2

--- main.py
+++ main.py
@@ -2,22 +2,41 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
 from constants import *
+from player import Player
+from asteroid import Asteroid
+import asteroidfield
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1==1:
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable)
+
+    field = asteroidfield.AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0)
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
-    
-    print(f"Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
